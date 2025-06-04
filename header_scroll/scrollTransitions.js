b0bd7ef8d6e90a6c5a9d9110a1db034647ebda46
Davide Turus <36860466+DTurus@users.noreply.github.com>
2025-06-04 17:08:08 +0200

    Create scrollTransitions.js

---
+++ header_scroll/scrollTransitions.js
@@ -0,0 +1,32 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+const headerHeight = document.getElementById("header").offsetHeight;
+let lastScrollPosition = 0;
+const delta = 350;
+
+function scrollFunction() {
+  let actualScrollPosition = window.scrollY;
+
+  if (
+    actualScrollPosition - lastScrollPosition <= delta &&
+    actualScrollPosition - lastScrollPosition >= 0
+  ) {
+    return;
+  }
+
+  if (
+    actualScrollPosition > headerHeight &&
+    actualScrollPosition >= lastScrollPosition
+  ) {
+    document.getElementById("header").style.position = "relative";
+  }
+
+  if (actualScrollPosition < lastScrollPosition) {
+    document.getElementById("header").style.position = "sticky";
+    document.getElementById("header").style.top = "0";
+  }
+
+  lastScrollPosition = actualScrollPosition;
+}
